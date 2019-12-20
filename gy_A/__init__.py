8cd43c12fb37c5505d14134712aeb70e02ff1c21
unknown <1334552035@qq.com>
2019-12-20 16:14:21 +0800

    first commit

--- gy_A/__init__.py
+++ gy_A/__init__.py
@@ -1,43 +1,77 @@
 #!/usr/bin/env python
-# -*- coding:utf-8 -*-
+#-*- coding:utf-8 -*-
 #json转字典
-# import json
-#
-# j = '''
-# {"name":"小明",
-# "sex":"未知"
-# }
-# '''
-# d = json.loads(j)
-# d["sex"] = "男"
-# print(d)
-# #字典转json
-# j = json.dumps(d,ensure_ascii=False)
-# print(j)
-#
-#import random
-#
-# a = random.randint(1,18)
-# print(a)
+import json
 
-# a = random.choice("dsdtdtd")
-# print(a)
+j = '''
+{"name":"小明",
+"sex":"未知"
+}
+'''
+d = json.loads(j)
+d["sex"] = "男"
+print(d)
+#字典转json
+j = json.dumps(d,ensure_ascii=False)
+print(j)
 
-# import os
-# # p = os.path.abspath("test.txt")
-# # print(p)
-# #
-# # d = os.path.dirname(p)
-# # print(d)
-# #
-# # f = os.path.basename(p)
-# # print(f)
-# #
-# # #拼接目录和文件
-# # path = os.path.join(d,f)
-# # print(path)
-# #
-# # os.mkdir("gy_C")
+import random
+
+a = random.randint(1,18)
+print(a)
+
+a = random.choice("dsdtdtd")
+print(a)
+
+import os
+p = os.path.abspath("test.txt")
+print(p)
+
+d = os.path.dirname(p)
+print(d)
+
+f = os.path.basename(p)
+print(f)
+
+#拼接目录和文件
+path = os.path.join(d,f)
+import json
+
+j = '''
+{"name":"小明",
+"sex":"未知"
+}
+'''
+d = json.loads(j)
+d["sex"] = "男"
+print(d)
+#字典转json
+j = json.dumps(d,ensure_ascii=False)
+print(j)
+
+import random
+
+a = random.randint(1,18)
+print(a)
+
+a = random.choice("dsdtdtd")
+print(a)
+
+import os
+p = os.path.abspath("test.txt")
+print(p)
+
+d = os.path.dirname(p)
+print(d)
+
+f = os.path.basename(p)
+print(f)
+
+#拼接目录和文件
+path = os.path.join(d,f)
+print(path)
+
+os.mkdir("gy_C")
 import os
 try:
      os.mkdir("gy_C")
@@ -47,5 +81,127 @@ else:
     print("文件夹创建成功")
 finally:
     print("创建文件夹结束")
-print("asdasd")
+print("asdasd")import json
+
+j = '''
+{"name":"小明",
+"sex":"未知"
+}
+'''
+d = json.loads(j)
+d["sex"] = "男"
+print(d)
+#字典转json
+j = json.dumps(d,ensure_ascii=False)
+print(j)
+
+import random
+
+a = random.randint(1,18)
+print(a)
+
+a = random.choice("dsdtdtd")
+print(a)
+
+import os
+p = os.path.abspath("test.txt")
+print(p)
+
+d = os.path.dirname(p)
+print(d)
+
+f = os.path.basename(p)
+print(f)
+
+#拼接目录和文件
+path = os.path.join(d,f)
+print(path)
+
+os.mkdir("gy_C")
+import os
+try:
+     os.mkdir("gy_C")
+except:
+    print("文件夹已存在")
+else:
+    print("文件夹创建成功")
+finally:
+    print("创建文件夹结束")
+print("asdasd")import json
+
+j = '''
+{"name":"小明",
+"sex":"未知"
+}
+'''
+d = json.loads(j)
+d["sex"] = "男"
+print(d)
+#字典转json
+j = json.dumps(d,ensure_ascii=False)
+print(j)
+
+import random
+
+a = random.randint(1,18)
+print(a)
+
+a = random.choice("dsdtdtd")
+print(a)
+
+import os
+p = os.path.abspath("test.txt")
+print(p)
+
+d = os.path.dirname(p)
+print(d)
+
+f = os.path.basename(p)
+print(f)
+
+#拼接目录和文件
+path = os.path.join(d,f)
+print(path)
+
+os.mkdir("gy_C")
+import os
+try:
+     os.mkdir("gy_C")
+except:
+    print("文件夹已存在")
+else:
+    print("文件夹创建成功")
+finally:
+    print("创建文件夹结束")
+print("asdasd")import json
+
+j = '''
+{"name":"小明",
+"sex":"未知"
+}
+'''
+d = json.loads(j)
+d["sex"] = "男"
+print(d)
+#字典转json
+j = json.dumps(d,ensure_ascii=False)
+print(j)
+
+import random
+
+a = random.randint(1,18)
+print(a)
+
+a = random.choice("dsdtdtd")
+print(a)
+
+import os
+p = os.path.abspath("test.txt")
+print(p)
+
+d = os.path.dirname(p)
+print(d)
+
+f = os.path.basename(p)
+print(f)
 
